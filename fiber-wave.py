d3228cf0cca70f8fe34f12f33c27ed799716e2bd
Alain <aalejo@pucp.edu.pe>
2019-12-14 09:29:59 -0500

    sort code

--- fiber-wave.py
+++ fiber-wave.py
@@ -5,49 +5,33 @@ import numpy as np
 
 def createFiberWave():
 
+    fiberSample = FiberSample(256, 256)
     # trazar una línea aleatoria con u radomness
+    points = fiberSample.createRandomLine()
+    print('Recta aleatoria:', points)
+
+    perp_points = fiberSample.getPerpendicular(points)
+    print('Recta perpendicular:', perp_points)
 
     # trazar onda senoidal
     # obtener la longitud de la recta aleatoria (time)
-    # generar amplitud
-    # obtener angulo de la recta aleatoria
-    # rotar
-
-    fiberSample = FiberSample(256, 256)
-
-    # img = fiberSample.createFiberSample(1, 5, 5)
-    # img.show()
-
-    points = fiberSample.createRandomLine()
-
-    print(points)
-
-    img = Image.new('RGB', (fiberSample.width, fiberSample.height), "black")
-    draw = ImageDraw.Draw(img)
-    draw.line(points, fill=(255, 255, 255), width=1)
-    #img.show()
-
-    perp_points = fiberSample.getPerpendicular(points)
-
-    print(perp_points)
-
-    draw.line(perp_points, fill=(255, 255, 255), width=1)
-
-    #img.show()
-
-    #return img
-
     distance = getDistance(perp_points)
     print('distance:', round(distance))
-
     time = np.arange(0, distance, 1)
-    amplitude = np.sin(0.01 * time)
+    print('time:', time)
 
+    # generar amplitud
+    amplitude = np.sin(0.01 * time)
+    print('amplitud:', amplitude)
+
+    # obtener angulo de la recta aleatoria
     x1, x2 = perp_points[0]
     y1, y2 = perp_points[1]
-
     m = (y2-y1)/(x2-x1)
-    angle = math.degrees(math.atan(m))
+    angle = math.atan(m)
+    print('angle:', math.degrees(angle))
+
+    # rotar
 
     xy_sine = []
     xy_points = []
@@ -58,27 +42,29 @@ def createFiberWave():
         xy_sine.append(time[i])
         xy_sine.append(mitad - round(a * pond))
 
-        point = rotate((0, 0), (time[i], round(a * pond)), math.radians(angle + 90))
+        point = rotate((0, 0), (time[i], round(a * pond)), angle)
 
         xy_points.append(point[0])
-        xy_points.append(mitad - point[1])
+        xy_points.append(point[1])
+
+        # Mostrando proceso a tráves de rectas
+        img = Image.new('RGB', (fiberSample.width, fiberSample.height), "black")
+        draw = ImageDraw.Draw(img)
+        draw.line(points, fill=(255, 255, 255), width=1)
+        draw.line(perp_points, fill=(255, 255, 255), width=1)
 
     draw.line(xy_points, fill=(255, 255, 255), width=2)
-
-    print('angle:', angle)
-
     img.show()
+
 
 def getDistance(line):
     x1, y1 = line[0]
     x2, y2 = line[1]
     return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
-
 def rotate(origin, point, angle):
     """
     Rotate a point counterclockwise by a given angle around a given origin.
-
     The angle should be given in radians.
     """
     ox, oy = origin
